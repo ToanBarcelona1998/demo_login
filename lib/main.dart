1aec0af4920a24e65f83de1c40c97f05b02584de
ToanBarcelona1998 <77067868+ToanBarcelona1998@users.noreply.github.com>
2021-02-24 15:08:20 +0700

    ok
    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,212 +1,240 @@
-import 'package:flutter/cupertino.dart';
+import 'package:demo_login/size_config.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
+class LoginPage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
-    double heightAppBar = MediaQuery.of(context).padding.top;
+    SizeConfig().init(context);
     return Scaffold(
       body: Container(
-        width: double.maxFinite,
-        height: double.maxFinite,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/login_background.png"),
-                fit: BoxFit.fill)),
-        child: SingleChildScrollView(
-          child: Column(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/login_background.png"),
+                  fit: BoxFit.fill)),
+          child: Stack(
             children: [
-              Container(
-                padding: EdgeInsets.only(top: heightAppBar),
-                child: Stack(
-                  children: [
-                    Positioned(
-                        left: 10,
-                        child: Text(
-                          "Cancle",
-                          style:
-                              TextStyle(fontSize: 20, color: Color(0xffeef2f8)),
-                        )),
-                    Container(
-                      alignment: Alignment.center,
-                      width: double.maxFinite,
-                      child: Text(
-                        "Registration",
-                        style:
-                            TextStyle(fontSize: 20, color: Color(0xffeef2f8)),
+              Positioned(
+                top: 0,
+                child: Container(
+                  height: SizeConfig.blockVertical * 4,
+                  width: SizeConfig.screenWidth,
+                  margin: EdgeInsets.only(top: SizeConfig.heightSafe),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: SizeConfig.blockHorizontal,
+                        child: Container(
+                          padding: EdgeInsets.all(4),
+                          child: Text(
+                            "Cancle",
+                            style: TextStyle(
+                                fontFamily: "Roboto-Regular",
+                                color: Color(0xffdce3f0),
+                                fontSize: SizeConfig.safeBlockHorizontal * 5),
+                          ),
+                        ),
                       ),
-                    ),
-                  ],
+                      Container(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Registration",
+                          style: TextStyle(
+                              fontFamily: "RobotoCondensed-Bold",
+                              color: Color(0xffdce3f0),
+                              fontSize: SizeConfig.safeBlockHorizontal * 5),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               Container(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: height / 18),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 1,
-                            child: Container(
-                              alignment: Alignment.centerRight,
-                              child: Text(
-                                "MQL",
-                                style: TextStyle(
-                                    fontSize: 25,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: "assets/fonts/Roboto-Bold.ttf",
-                                    color: Color(0xfff7fafc)),
+                height: SizeConfig.screenHeight -
+                    SizeConfig.heightSafe -
+                    SizeConfig.blockVertical * 4,
+                width: SizeConfig.screenWidth,
+                margin: EdgeInsets.only(
+                    top: SizeConfig.heightSafe + SizeConfig.blockVertical * 4),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(
+                            left: SizeConfig.safeBlockHorizontal * 16,
+                            right: SizeConfig.safeBlockHorizontal * 16),
+                        height: SizeConfig.blockVertical * 20,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "MQL",
+                                  style: TextStyle(
+                                    fontFamily: "RobotoCondensed-Bold",
+                                    color: Color(0xfff6f8fc),
+                                    fontSize:
+                                        SizeConfig.safeBlockHorizontal * 7,
+                                  ),
+                                ),
+                                Text(
+                                  "5",
+                                  style: TextStyle(
+                                    fontFamily: "RobotoCondensed-Bold",
+                                    color: Color(0xfff6d82e),
+                                    fontSize:
+                                        SizeConfig.safeBlockHorizontal * 8,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              "Join Traders Community www.mql5.com",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: "Roboto-Regular",
+                                color: Color(0xfff5f7fb),
+                                fontSize: SizeConfig.safeBlockHorizontal * 6,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Color(0xffffffff),
+                            borderRadius: BorderRadius.circular(
+                                SizeConfig.blockHorizontal)),
+                        margin: EdgeInsets.only(
+                            left: SizeConfig.safeBlockHorizontal * 16,
+                            right: SizeConfig.safeBlockHorizontal * 16),
+                        //height: SizeConfig.blockVertical * 20,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            TextField(
+                              textAlign: TextAlign.right,
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Enter mail".toLowerCase(),
+                                  contentPadding: EdgeInsets.all(
+                                      SizeConfig.blockHorizontal * 2),
+                                  hintStyle: TextStyle(
+                                    fontSize:
+                                        SizeConfig.safeBlockHorizontal * 5,
+                                    color: Color(0xffe4e6e8),
+                                  )),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(
+                                  SizeConfig.blockHorizontal * 2),
+                              child: Divider(
+                                color: Colors.grey,
                               ),
                             ),
+                            TextField(
+                              textAlign: TextAlign.right,
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Enter pass".toLowerCase(),
+                                  contentPadding: EdgeInsets.all(
+                                      SizeConfig.blockHorizontal * 2),
+                                  hintStyle: TextStyle(
+                                    fontSize:
+                                        SizeConfig.safeBlockHorizontal * 5,
+                                    color: Color(0xffe4e6e8),
+                                  )),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(SizeConfig.safeBlockHorizontal*2),
+                        margin: EdgeInsets.only(
+                            left: SizeConfig.safeBlockHorizontal * 16,
+                            right: SizeConfig.safeBlockHorizontal * 16),
+                        height: SizeConfig.blockVertical * 12,
+                        alignment: Alignment.center,
+                        child: Text(
+                          "The password for your MQL5 account will be sent to the specifield email address",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: SizeConfig.safeBlockHorizontal * 4.5,
+                            color: Color(0xffc8d4e7),
+                            fontFamily: "Roboto-Regular",
                           ),
-                          Expanded(
-                            flex: 1,
-                            child: Container(
-                              child: Text(
-                                "5",
-                                style: TextStyle(
-                                  color: Color(0xfff2d631),
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(
+                            left: SizeConfig.safeBlockHorizontal * 16,
+                            right: SizeConfig.safeBlockHorizontal * 16),
+                        height: SizeConfig.blockVertical * 22,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              height: SizeConfig.blockVertical * 8,
+                              width: double.maxFinite,
+                              color: Color(0xfffab624),
+                              child: FlatButton(
+                                onPressed: () {},
+                                child: Text(
+                                  "Register",
+                                  style: TextStyle(
+                                      fontSize:
+                                          SizeConfig.safeBlockHorizontal * 4,
+                                      fontFamily: "Roboto_Bold"),
                                 ),
                               ),
                             ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(
-                          bottom: height / 18, top: height / 28),
-                      alignment: Alignment.centerLeft,
-                      width: width * 3 / 4,
-                      child: Text(
-                        "Join Traders Community www.mql5.com",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 22,
-                            color: Color(0xffc7d4e9),
-                            fontFamily: "assets/fonts/Roboto-Regular.ttf"),
-                      ),
-                    ),
-                    Container(
-                      width: width * 3 / 4,
-                      decoration: BoxDecoration(
-                        color: Color(0xffffffff),
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: Column(
-                        children: [
-                          TextField(
-                            textAlign: TextAlign.right,
-                            decoration: InputDecoration(
-                                contentPadding: EdgeInsets.all(12),
-                                border: InputBorder.none,
-                                hintText: "Enter mail"),
-                          ),
-                          Divider(
-                            color: Colors.grey,
-                          ),
-                          TextField(
-                            textAlign: TextAlign.right,
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Enter pass",
-                              contentPadding: EdgeInsets.all(12),
+                            Text(
+                              "or",
+                              style: TextStyle(
+                                  color: Color(0xffbccbe3),
+                                  fontSize: SizeConfig.safeBlockHorizontal * 5),
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 10, bottom: 10),
-                      width: width * 3 / 4,
-                      alignment: Alignment.center,
-                      child: Text(
-                        "The password for your MQL5 account will be sent to the specifield email address",
-                        style:
-                            TextStyle(fontSize: 16, color: Color(0xffbecde5),fontFamily: "assets/fonts/RobotoCondensed-Regular.ttf"),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    Container(
-                      width: width * 3 / 4,
-                      height: height/14,
-                      child: FlatButton(
-                        color: Color(0xfffab624),
-                        onPressed: () {},
-                        child: Text(
-                          "Register",
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                        padding: EdgeInsets.all(5),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        "Or",
-                        style:
-                            TextStyle(fontSize: 16, color: Color(0xffa6bad9)),
-                      ),
-                    ),
-                    Container(
-                      height: height/14,
-                      width: width * 3 / 4,
-                      child: FlatButton(
-                        color: Color(0xffdee6f3),
-                        onPressed: () {},
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
                             Container(
-                              child: Image.asset(
-                                "assets/images/facebook.png",
-                                height: 20,
-                              ),
-                            ),
-                            SizedBox(width: 10,),
-                            Container(
-                              child: Text(
-                                "Continue with Facebook",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              height: SizeConfig.blockVertical * 8,
+                              color: Color(0xffdee6f3),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset("assets/images/facebook.png",height: SizeConfig.blockVertical*4,),
+                                  Padding(padding: EdgeInsets.all(SizeConfig.blockHorizontal)),
+                                  Text("Continue with Facebook",style: TextStyle(
+                                      fontSize:
+                                      SizeConfig.safeBlockHorizontal * 4,
+                                      fontFamily: "Roboto_Bold"),)
+                                ],
                               ),
                             ),
                           ],
                         ),
-                        padding: EdgeInsets.all(5),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ],
-          ),
-        ),
-      ),
+          )),
     );
   }
 }
